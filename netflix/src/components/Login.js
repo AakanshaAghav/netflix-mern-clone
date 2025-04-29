9937483460dac68850642e326aef560476d0851e
AakanshaAghav <158139858+AakanshaAghav@users.noreply.github.com>
2025-04-29 11:53:39 +0530

    Update Login.js

--- netflix/src/components/Login.js
+++ netflix/src/components/Login.js
@@ -1,5 +1,4 @@
 import React, {useState} from 'react';
-//import { useNavigate } from 'react-router-dom';
 import Header from './Header';
 import axios from 'axios';  
 import { API_END_POINT } from '../utils/constant';
